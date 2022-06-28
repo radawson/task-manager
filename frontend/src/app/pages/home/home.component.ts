47d61714f947073a110ddb4db71c4f05947daf89
Rick Dawson <dawsora@gmail.com>
2022-06-28 13:17:33 -0400

    Fixed Task View

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { TaskService } from 'src/app/task.service';
+import { Task } from 'src/app/models/task.model';
+import { List } from 'src/app/models/list.model';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  lists!: List[];
+  tasks!: Task[];
+  taskView: boolean = false;
+  day: String = "Sunday";
+
+  constructor(private taskService: TaskService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const d = new Date();
+    this.day = weekday[d.getDay()];
+
+    this.route.params.subscribe(
+      (params: Params) => {
+        console.log(params);
+        if (params['listId']) {
+          this.taskService.getTasks(params['listId']).subscribe((tasks: any) => {
+            this.tasks = tasks;
+            this.taskView = true;
+          })
+        }
+      })
   }
 
+  onTaskClick(task: Task) {
+    this.taskService.complete(task).subscribe(() => {
+      task.completed = !task.completed;
+    });
+  }
 }
+
+
